5ea1fb0acef187cedd5410c5af18c072ef3dc8a3
wangbiwang <biwang.wang@arvato.com>
2023-03-06 11:42:59 +0800

    ✨ feat(ref): 实现ref简单数据类型的响应性

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,5 +1,6 @@
+import { hasChanged } from '@vue/shared'
 import { Dep, cerateDep } from './dep'
-import { activeEffect, trackEffects } from './effect'
+import { activeEffect, trackEffects, triggerEffects } from './effect'
 import { toReactive } from './reactive'
 
 export interface Ref<T = any> {
@@ -16,20 +17,33 @@ function createRef(rawValue: unknown, shallow: boolean) {
 }
 class RefImpl<T> {
     private _value: T
+    private _rawValue: T
     public dep?: Dep = undefined
     public readonly __v_isRef = true
     constructor(value: T, public readonly __v_isShallow: boolean) {
         this._value = __v_isShallow ? value : toReactive(value)
+        this._rawValue = value
     }
     get value() {
         trackRefValue(this)
         return this._value
     }
-    set value(newVal) {}
+    set value(newVal) {
+        if (hasChanged(newVal, this._rawValue)) {
+            this._rawValue = newVal
+            this._value = toReactive(newVal)
+            triggerRefValue(this)
+        }
+    }
 }
 function trackRefValue(ref) {
     if (activeEffect) {
         trackEffects(ref.dep || (ref.dep = cerateDep()))
+    }
+}
+function triggerRefValue(ref) {
+    if (ref.dep) {
+        triggerEffects(ref.dep)
     }
 }
 /**
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,2 +1,6 @@
 export const isArray = Array.isArray
 export const isObjeact = (val: unknown) => val !== null && typeof val === 'object'
+/**
+ * 对比数据是否发生变化
+ */
+export const hasChanged = (value: unknown, oldValue: unknown): boolean => !Object.is(value, oldValue)
